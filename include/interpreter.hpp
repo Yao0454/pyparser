a082eab8054b21f4b7185b463dbf6a4e35a05f95
Yao0454 <yaoyifeng20070524@outlook.com>
2026-06-18 15:43:59 +0800

    Introduce Value type and implement eval/exec

--- include/interpreter.hpp
+++ include/interpreter.hpp
@@ -3,14 +3,30 @@
 
 #include "parser.hpp"
 #include <cstddef>
+#include <string>
 
+enum class ValueType {
+    NUMBER,
+    STRING,
+    BOOLEAN,
+};
+
+struct Value {
+    ValueType type;
+    int number = 0;
+    std::string str;
+    bool boolean = false;
+};
+
+Value eval(const Node &node);
+void exec(const Node &node);
+
+void iter_node(const Node &node);
 void itpt_funcdef(const Node &node);
 void itpt_block(const Node &node);
 void itpt_assign(const Node &node);
-void iter_node(const Node &node);
-void itpt_ident(const Node &node);
-int *itpt_number(const Node &node);
-void itpt_if(const Node &node);
+void itpt_number(const Node &node);
+Value itpt_if(const Node &node);
 void itpt_binop(const Node &node);
 void itpt_call(const Node &node);
 void itpt_string(const Node &node);
--- source/interpreter.cpp
+++ source/interpreter.cpp
@@ -5,7 +5,89 @@
 #include <unordered_map>
 #include <vector>
 
-std::unordered_map<std::string, void *> ident_pool;
+std::unordered_map<std::string, Value> variebles;
+
+Value eval(const Node &node) {
+    switch (node.type) {
+    case NodeType::NUMBER:
+        return Value{
+            ValueType::NUMBER,
+            stoi(node.value),
+            "",
+            false,
+        };
+    case NodeType::IDENT:
+        if (node.value == "True") {
+            return Value{
+                ValueType::BOOLEAN,
+                0,
+                "",
+                true,
+            };
+        } else if (node.value == "False") {
+            return Value{
+                ValueType::BOOLEAN,
+                0,
+                "",
+                false,
+            };
+        } else {
+            return variebles[node.value];
+        }
+    case NodeType::IF: {
+        Node op = node.children[0];
+        Node block = node.children[1];
+    }
+    default:
+        assert(false && "[Error]: UNREABHABLE!");
+    }
+    assert(false && "function eval is not implemented yet");
+}
+
+void exec(const Node &node) {
+    switch (node.type) {
+    case NodeType::ASSIGN: {
+        Node l = node.children[0];
+        Node r = node.children[1];
+        assert(l.type == NodeType::IDENT);
+        if (r.type == NodeType::NUMBER) {
+            variebles[l.value] = Value{
+                ValueType::NUMBER,
+                stoi(r.value),
+                "",
+                false,
+            };
+        } else if (r.type == NodeType::STRING) {
+            variebles[l.value] = Value{
+                ValueType::STRING,
+                0,
+                r.value,
+                false,
+            };
+        } else if (r.type == NodeType::IDENT) {
+            if (r.value == "True") {
+                variebles[l.value] = Value{
+                    ValueType::BOOLEAN,
+                    0,
+                    "",
+                    true,
+                };
+            } else if (r.value == "False") {
+                variebles[l.value] = Value{
+                    ValueType::BOOLEAN,
+                    0,
+                    "",
+                    false,
+                };
+            } else {
+                variebles[l.value] = variebles[r.value];
+            }
+        }
+    } break;
+    default:
+        assert(false && "function exec is not implemented yet");
+    }
+}
 
 void itpt_funcdef(const Node &node) {
     for (const Node &child : node.children) {
@@ -19,34 +101,14 @@ void itpt_block(const Node &node) {
     }
 }
 
-void itpt_assign(const Node &node) {
-    itpt_ident(node.children[0]);
-    switch (node.children[1].type) {
-    case NodeType::NUMBER:
-        itpt_number(node.children[1]);
-        break;
-    case NodeType::STRING:
-        itpt_string(node.children[1]);
-        break;
-    default:
-        assert(false && "Can not assign this NodeType!");
-    }
-}
+void itpt_assign(const Node &node) { exec(node); }
 
-void itpt_ident(const Node &node) {
-    (void)node;
-    assert(false && "interprete ident is not implemented yet!");
-}
-
-int *itpt_number(const Node &node) {
+void itpt_number(const Node &node) {
     (void)node;
     assert(false && "interprete number is not implemented yet!");
 }
 
-void itpt_if(const Node &node) {
-    (void)node;
-    assert(false && "interprete if is not implemented yet!");
-}
+Value itpt_if(const Node &node) { return eval(node); }
 
 void itpt_binop(const Node &node) {
     (void)node;
@@ -73,9 +135,6 @@ void iter_node(const Node &node) {
         break;
     case NodeType::ASSIGN:
         itpt_assign(node);
-        break;
-    case NodeType::IDENT:
-        itpt_ident(node);
         break;
     case NodeType::NUMBER:
         itpt_number(node);
